6ed8242dd9cffac390fa23114e054c2b0b20d281
Gábor Csepregi <csepregigabor@outlook.com>
2017-06-12 19:39:31 +0200

    add first task

--- ProcessNote/Form1.cs
+++ ProcessNote/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+using System.Dynamic;
 
 namespace ProcessNote
 {
@@ -16,5 +19,57 @@ namespace ProcessNote
         {
             InitializeComponent();
         }
-    }
-}
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            Process process = new Process();
+            process.StartInfo.FileName = text;         
+            process.Start();  
+            loadProcessList();
+           
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadProcessList();                  
+
+            }
+
+        private void loadProcessList()
+        {
+            listView1.Items.Clear();
+            Process[] processList = Process.GetProcesses();
+            foreach (Process process in processList)
+            {
+                ListViewItem item = new ListViewItem(process.ProcessName);
+                item.Tag = process;
+                listView1.Items.Add(item);
+                
+                
+            }
+            
+        }
+
+        
+
+       
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+           ListViewItem item =  listView1.SelectedItems[0];
+            
+            Process process = (Process)item.Tag;
+            process.Kill();
+            loadProcessList();
+        }
+
+        }
+
+       
+            
+        }
+    
+
