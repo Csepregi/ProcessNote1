09b681e26f89a8415ac1b9dce16fc1b2cd5a73a5
Gábor Csepregi <csepregigabor@outlook.com>
2017-06-13 21:09:15 +0200

    second task

--- ProcessNote/Form1.Designer.cs
+++ ProcessNote/Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -56,7 +62,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(206, 241);
+            this.textBox1.Location = new System.Drawing.Point(52, 241);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(152, 20);
             this.textBox1.TabIndex = 3;
@@ -64,23 +70,70 @@
             // listView1
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1});
-            this.listView1.Location = new System.Drawing.Point(99, 12);
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 28);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(391, 207);
+            this.listView1.Size = new System.Drawing.Size(157, 207);
             this.listView1.TabIndex = 4;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // columnHeader1
             // 
             this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ID";
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView2.Location = new System.Drawing.Point(320, 12);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(321, 236);
+            this.listView2.TabIndex = 5;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Memory Usage";
+            this.columnHeader5.Width = 91;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Tag = "";
+            this.columnHeader6.Text = "Running Time";
+            this.columnHeader6.Width = 120;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Start Time";
+            this.columnHeader7.Width = 104;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "details";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(664, 336);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView2);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -98,6 +151,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
 
--- ProcessNote/Form1.cs
+++ ProcessNote/Form1.cs
@@ -11,65 +11,87 @@ using System.Diagnostics;
 using System.Management;
 using System.Dynamic;
 
+
 namespace ProcessNote
 {
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
+            listView1.FullRowSelect = true;
+
         }
 
         private void startButton_Click(object sender, EventArgs e)
         {
             string text = textBox1.Text;
             Process process = new Process();
-            process.StartInfo.FileName = text;         
-            process.Start();  
+            process.StartInfo.FileName = text;
+            process.Start();
             loadProcessList();
-           
         }
-
-        
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            loadProcessList();                  
-
-            }
+            loadProcessList();
+        }
 
         private void loadProcessList()
         {
+
             listView1.Items.Clear();
+            listView1.FullRowSelect = true;
             Process[] processList = Process.GetProcesses();
             foreach (Process process in processList)
             {
+
                 ListViewItem item = new ListViewItem(process.ProcessName);
+                item.SubItems.Add(process.Id.ToString());
+               
                 item.Tag = process;
                 listView1.Items.Add(item);
-                
-                
             }
-            
         }
 
-        
-
-       
 
         private void stopButton_Click(object sender, EventArgs e)
         {
-           ListViewItem item =  listView1.SelectedItems[0];
-            
+            ListViewItem item = listView1.SelectedItems[0];
+
             Process process = (Process)item.Tag;
             process.Kill();
             loadProcessList();
         }
 
-        }
 
-       
-            
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listView2.Items.Clear();
+            //listView2.FullRowSelect = true;
+            Process[] processList = Process.GetProcesses();
+            foreach (Process process in processList)
+            {
+
+
+                ListViewItem item = new ListViewItem();
+                //item.SubItems.Add(process.PagedMemorySize64.ToString());
+                item.SubItems.Add((DateTime.UtcNow - Process.GetCurrentProcess().StartTime.ToUniversalTime()).ToString());
+
+                //item.SubItems.Add((System.Diagnostics.Process.GetProcessesByName(process.ProcessName).FirstOrDefault().StartTime).ToString());
+                item.SubItems.Add(Process.GetCurrentProcess().StartTime.ToString());
+
+
+                item.Tag = process;
+                listView2.Items.Add(item);
+                //listView2.Items.Add((process.PagedMemorySize64.ToString()));
+                //listView2.Items.Add((DateTime.UtcNow - Process.GetCurrentProcess().StartTime.ToUniversalTime()).ToString());
+            }            
         }
+    }
+}
+ 
     
 
